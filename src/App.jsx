2d49721b0c5b17110797d3ac4eed1b3df65fd56e
blessingsmikuwa <bed-phy-55-21@unima.ac.mw>
2026-03-15 11:02:45 +0200

    Initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-600">
+      <h1 className="text-white text-4xl font-bold">
+        E-Library System
+      </h1>
+    </div>
+  )
+}
+
+export default App
